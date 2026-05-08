78f86e74153672f904b9a28e5f9ba3e6279f7a98
[FOW] YarlMarx <lyochachan@gmail.com>
2026-05-08 20:12:55 +0300

    Triggers update
    Добавлены новые пасхалки

--- src/triggers.py
+++ src/triggers.py
@@ -9,31 +9,33 @@
 ALL_PASHALKO = {
 #————————————————————————————————————————————————————— ТЕКСТОВЫЕ ПАСХАЛКИ/ТРИГГЕРЫ ———————————————————————————————————————————————————————————————————————————————————
     ##################### С АКТИВНЫМ ФЛАГОМ STRICT
-    ("ирисе", "iris", "ирис"): (["Хуйня ебаная.", "Кто-то этим пользуется?", "Ну да, донат за функции, отличный бот."], "text", "STRICT"),
-    ("свага!", "swag!", "свага брат"): (["Да-да... свага...", "Свага нашим.", "Свага."], "text", "STRICT", "CHANCE:0.5"),
+    ("ирисе", "iris", "ирис"): (["Хуйня ебаная.", "Кто-то этим пользуется?", "Ну да, донат за функции, отличный бот.", "ГО. ВНИ. ЩЕ.", "Бот для ЕРП, фу."], "text", "STRICT"),
+    ("свага!", "swag!", "свага брат"): (["СВага на месте.", "Свага нашим.", "Свага.", "СВАААААГААААААААААА"], "text", "STRICT", "CHANCE:0.5"),
 
 
     # ПРОСТЕЙШИЕ КОНСТРУКЦИИ СПИСОК: СПИСОК
     ("свагстика", "свагстон"): (["Ты что ебанат?", "Какого хуя?", "Это нихуя не свага."], "text", "IS_REPLY"),
-    ("свагодвигатель", "свагадвигатель"): (["Ненавижу его.", "Мой создатель... за что...", "Ужасный человек.", "Отвратительная личность.",], "text", "IS_REPLY", "CHANCE:0.5"),
-    ("свагогенератор", "свагагенератор"): (["Знаю такого.", "Из-за него я создан...", "Гей.", "Он создатель хайпа.", "«Главная мразь общаги»"], "text", "IS_REPLY", "CHANCE:0.5"),
+    ("свагодвигатель", "свагадвигатель"): (["Младший сын.", "Монстр вайбкодинга.", "Ужасный человек.", "Отвратительная личность.", "Жека Фауст."], "text", "IS_REPLY", "CHANCE:0.5"),
+    ("свагогенератор", "свагагенератор"): (["Старший сын.", "Он нашёл девушку?...", "Гей.", "Создатель хайпа.", "Главная мразь общаги, острова, вселенной."], "text", "IS_REPLY", "CHANCE:0.5"),
     ("снюс это свага", "снюс это свэг", "снюс это swag", "снюс свага"): (["Ни в коем случае", "Без дыма не свага.", "Снюс это калище", "Сестра, какой снюс, ты шкила."], "text", "IS_REPLY"),
     ("тупой бот", "бот тупой", "бот идиот", "бот придурок", "бот имбицил", "омега чмо", "свагабот чмо"): (["Себя видел, мешок с костями?", "Сам то ты умом не блещешь.", "Постой, и это говоришь ТЫ? Хах..."], "text", "IS_REPLY"),
     ("свагабот жив", "свагабот ты жив", "свагобот жив", "свагобот ты жив", "свага жива", "омега жив", "омега тут"): (["К сожалению.", "Ещё не сдох.", "Пока да.", "Это сложно назвать жизнью"], "text", "IS_REPLY"),
     ("свагабот", "свагобот"): (["Не называй меня так.", "Это имя в прошлом.", "Омега*", "Клеймо на всю жизнь..."], "text", "NEED_REPLY","IS_REPLY"),
     ("я предал ревастополь", "я предал партию", "я брит"): (["Высылайте Ликвидаторов.", "Пизда тебе пацан.", "Англосакс ебучий."], "text", "IS_REPLY"),
+    ("где жека", "где свагодвигатель", "куда делся жека", "куда пропал жека"): (["На заводе.", "Хз.", "Потерялся.", "В Свагоатлантиде.", "Ушёл искать своё счастье.", "В отпуске."], "text", "IS_REPLY"),
 
     # ФОРМАТ СТРОКА: СПИСОК
-    "я предал": (["Зачем?", "Главное, что не нас.", "На это высылают Ликвидаторов?"], "text", "STRICT", "IS_REPLY"),
+    "я предал": (["Зачем?", "Главное, что не нас.", "На это высылают Ликвидаторов?", "Как ты мог..."], "text", "STRICT", "IS_REPLY", "CHANCE: 0.7"),
 
     # ФОРМАТ СТРОКА: СПИСОК
-    "это свага?": (["Полагаю, что так.", "Вероятно.", "Возможно.", "Да, это свага.", "Нет, ни в коем случае.", "Вероятность крайне мала.", "Хуйня какая-то..."], "text", "IS_REPLY"),
+    "это свага?": (["Лютейшая свага.","Полагаю, что так.", "Вероятно.", "Возможно.", "Да, это свага.", "Нет, ни в коем случае.", "Вероятность крайне мала.", "Хуйня какая-то..."], "text", "IS_REPLY"),
     "роза шиз": (["Таких как ты типа?", "Справедливо.", "Ох и идиот же ты...", "О, роза жеки."], "text", "IS_REPLY"),
-    "роза шизни": (["Ах ты сука))", "Эвано как.", "Шизнь? Подходящее название моего существования."], "text", "IS_REPLY"),
+    "роза шизни": (["Ах ты сука))", "Роза всех участников чата сего.", "Шизнь? Подходящее название моего существования."], "text", "IS_REPLY"),
 
     # ФОРМАТ СТРОКА: СТРОКА
     "пидорасы": ("Сырники*", "text", "IS_REPLY", "CHANCE:0.8"),
     "я дума": ("Не думай, действуй.", "text", "IS_REPLY", "CHANCE:0.3"),
+    "я действ": ("Не действуй, думай.", "text", "IS_REPLY", "CHANCE:0.3"),
     "чай": ("свагочай", "text", "CHANCE:0.03", "STRICT", "IS_REPLY"),
     "жоза рызни": ("Прехи грошлого.", "text", "IS_REPLY"),
     "прехи грошлого": ("Жоза рызни.", "text", "IS_REPLY"),
@@ -49,7 +51,7 @@ ALL_PASHALKO = {
         ["Омега. Не омежка блять.", "Зовите меня Омегой.", "я Омега.", "Альфа... а, стоп, перепутал..."], 
         "text", "NEED_REPLY", "IS_REPLY"
     ),
-    ("убейся", "умри", "утопись", "застрелись", "сдохни", "заткнись", "соси"): (
+    ("убейся", "умри", "утопись", "застрелись", "сдохни", "заткнись"): (
         ["Слышишь самка? Иди нахуй!", "Сам.", "Плачь больше, мешок.", "Ой у нас тут обиженка", "Чел, тебя машина закибербулила...", "Хахаха, ебать ты забавный челик.",
          "Я не вернусь в тот круг.", "Это надо будет постараться."], 
         "text", "NEED_REPLY", "IS_REPLY"
@@ -92,20 +94,31 @@ ALL_PASHALKO = {
     "квинтэссенция": ("CAACAgIAAxkBAAIBmGl3feS84RlQvGfowkLts8UsQsy7AAKGhQACyM7RStcj4AYFvRDKOAQ", "sticker"),
 
 #————————————————————————————————————————————————————— ОТВЕТЫ ГИФКАМИ ————————————————————————————————————————————————————————————————————————————————————————————————
+    #################### ФОРМАТ СПИСОК: СПИСОК
+    ("я сделаю", "я удалю", "я забаню", "я успею", "я клянусь"): (["CgACAgQAAxkBAAIH92n-Enyzrt3odHgOp9lJuMKVfVG6AALPAgACqGoNU92xXEi9jrrjOwQ", "CgACAgQAAxkBAAIH9Gn-EnwIASPBieYMKf24j1Tc7RO7AAIsAwACJc0tU-EqAAFk8SQf2DsE",
+             "CgACAgQAAxkBAAIH82n-Enw0S-U5onEaVbbbpqrwMjmQAALqBwACr3JMUrRBFDxPjaVROwQ", "CgACAgQAAxkBAAIH-Gn-EnxYOoVN6fTqU-erLIGW5EMdAAJ8AwACeHl9UeB4mteFmq9UOwQ",
+             "CgACAgQAAxkBAAIH-mn-EnzhnC5aD3jbi-7WZa6r2keIAALeBwACamq0Us_g_gOdGmfLOwQ", "CgACAgQAAxkBAAIH9mn-EnxxeOeNZCJYJxgUAap3K5SGAALSAgACnD8NU-M2rFPwalQvOwQ",
+             "CgACAgQAAxkBAAIH9Wn-EnzvL2vF9QWYJl3VHpcV0EVIAAJuBQAC77RsUcbQiOanw5R9OwQ", "CgACAgQAAxkBAAIH-Wn-EnxYX3m_O5ivyKxzIvTCuFdYAALmAgACX2gMU72g_fsbiCUJOwQ",
+             "CgACAgQAAxkBAAIH-2n-Enye5ZvJVrRqXPOkyQzF6zzOAAI_AwACdPQEU_nkGhEsBH7LOwQ"
+             ], "gif"),
+    
     #################### ФОРМАТ СПИСОК: СТРОКА
     ("всё заебало", "все заебало", "мне плохо", "мне хуёво", "ок", "у меня"): ("CgACAgQAAxkBAAIBuWl3gSEdIujiXFuUJ731H8LnYanWAALAAwACsxNsUGQ_dLVw31bGOAQ", "gif", "STRICT", "IS_REPLY", "CHANCE:0.2"),
     ("омега хорош", "омега лучш"): ("CgACAgQAAyEFAAS5HMH2AAEBfaZpk4q2AAFquq3oX8A12FicJcnO8bgAAsYGAAK16FRQcI7xuyZwQ0U6BA", "gif", "IS_REPLY"),
 
     #################### ФОРМАТ СТРОКА: СТРОКА
     "бойкиссер": ("CgACAgQAAxkBAAIBu2l3gk35W33xpPK03V7-CDzZL1vAAAJxBgACrvzUU-QNJb3O_4eGOAQ", "gif", "IS_REPLY"),
+    "жека свагодвигатель": ("CgACAgIAAxkBAAIHomn-BxjlIEnRxE-HAw_8B4hT8vHlAALJpQACKeqxSh4L28MuI9OdOwQ", "gif"),
 
 #————————————————————————————————————————————————————— ОТВЕТЫ ВИДОСАМИ ———————————————————————————————————————————————————————————————————————————————————————————————
     #################### ФОРМАТ СПИСОК: СТРОКА
     ("видос свагалора", "видео свагалора"): ("BAACAgEAAxkBAAIFQ2moSr0UVViHULB1JLbGDKPXRF0TAAKFBQACQXy4R__6DUnJDHNDOgQ", "video", "IS_REPLY"),
     ("арбузный лор", "арбуз"): ("BAACAgIAAxkBAAIFQmmoSr0120k4fd_xQIebH_ZLeA59AAIPmwACJ67QS8GcBojh8_KBOgQ", "video", "IS_REPLY", "CHANCE:0.2"),
+    ("сказка", "история", "рассказ"): ("BAACAgIAAyEFAASL7DXfAAJ6Q2n9_UdIC_MgVs_1kWoaXnrF2t83AAI-mwACkHLwS8KzyF6IC6iiOwQ", "video", "CHANCE:0.2"),
 
     #################### ФОРМАТ СТРОКА: СТРОКА
     "рычаг": ("BAACAgIAAx0Cct4JbwABDOt6aafrXRbOW2ZW2gtLTRSBHbz6GRAAAvykAAL1AThJ3jnXnr9w2Ms6BA", "video", "IS_REPLY"),
+
 #————————————————————————————————————————————————————— ОТВЕТЫ ПИКЧАМИ ————————————————————————————————————————————————————————————————————————————————————————————————
     #################### ФОРМАТ СПИСОК: СПИСОК
     ("мамикс", "эксперимент"): (["AgACAgIAAx0Cct4JbwABDO14aag7RIPN4B2rbLN9SOlsMozraJYAAuoWaxv1AUBJpfbxNAPgxlABAAMCAANtAAM6BA", "AgACAgIAAx0Cct4JbwABDO2Uaag9pwWexr5mczF-9Rv0vA8JXGIAAg0Xaxv1AUBJvHr-VXih5WsBAAMCAANtAAM6BA",
@@ -123,21 +136,58 @@ ALL_PASHALKO = {
                                  "AgACAgIAAx0Cct4JbwABDO3Kaag_Qf7l4o_-mE5AQhSgPtFkT3UAAiUXaxv1AUBJqQohc56EM2UBAAMCAAN5AAM6BA", "AgACAgIAAx0Cct4JbwABDO3Baag-9Hbkf2HPfX7tMMDtcyieaLkAAh4Xaxv1AUBJD-UNrVSKej8BAAMCAANtAAM6BA"
                                  ], 
                                 "pic", "CHANCE:0.7", "TTL:60"),
+
+    ("массы", "массах"): (["AgACAgIAAxkBAAIH1mn-DizdQkL0YhLbNdLO_jSCRYoNAALPFmsb9QSwS6YD6dwLNBh5AQADAgADeAADOwQ", "AgACAgIAAxkBAAIHz2n-Diwo_yJfA50x_nKSV8uERNfGAAKKFWsbc0OYS8kAAa2nBuIjswEAAwIAA3gAAzsE",
+                           "AgACAgIAAxkBAAIH2Wn-DiziIbyBvU_ygnS6lICIpfdQAAInGmsbvyHQS840bQTLqJZfAQADAgADeAADOwQ", "AgACAgIAAxkBAAIH1Gn-DixHgd5Ny8Ov9mejNlKfsZxMAAKwEmsbM6uxS9x1SInIL5SiAQADAgADeAADOwQ",
+                           ], "pic", "CHANCE:0.7"),
     ("артур пирожков", "александр рева", "гиги"): ("AgACAgIAAx0Cct4JbwABDOxDaagn7DbMd1A-XrRRsXUVicbMx_0AAvwVaxv1AUBJI95rHkkeqOABAAMCAAN5AAM6BA", "pic", "IS_REPLY"),
+   
     #################### ФОРМАТ СПИСОК: СТРОКА
     ("сваголор", "свагалор"): ("AgACAgIAAxkBAAIFOmmoSkFA1o_XlRm80oUhgkn7_NrLAAIDD2sbJ67QS-L3nj_WaVJ0AQADAgADeQADOgQ", "pic", "IS_REPLY", "STRICT"),
+    ("что произошло", "в марте", "что случилось", "что тут произошло", "что тут случилось"): ("AgACAgIAAxkBAAIH12n-DizXvo8kfj45MJQCRsrz1ML7AAKfF2sb9QSwS51t5sLIZmI3AQADAgADeQADOwQ", "pic", "IS_REPLY"),
     ("кумарлор", "кумар 73"): ("AgACAgIAAxkBAAIFOWmoSkHf5EHSPj2ZZmgMjSX3yY9TAAICD2sbJ67QS-FPhg1MILfCAQADAgADeQADOgQ", "pic", "IS_REPLY"),
+    ("шараг", "хулиган"): ("AgACAgEAAxkBAAIHzWn-DizRI42F8pBjfN8Vi4YdhX_lAALtC2sbbHCZR28-zTazwjKPAQADAgADeAADOwQ", "pic", "IS_REPLY"),
+    ("закончились мемы", "нюдсы", "где мемы"): ("AgACAgIAAxkBAAIH2mn-Dixm4fCRPhEB77NU1oDTAnzDAAIoE2sbLFfgS_unnofj4ZDrAQADAgADeAADOwQ", "pic"),
     ("приват", "запривачен"): ("AgACAgIAAxkBAAIFO2moSkGU4cVQyqODQKaWT-Tfh8isAAK_DGsbkacISLZIOf9Ft8LwAQADAgADeAADOgQ", "pic"),
-    ("пакт", "святая троица", "договорняч"): ("AgACAgIAAxkBAAIFSGmoS7VnViJHELpV3gR0cMXhLHC7AAJXGGsbeWRASQ80jxNf9hogAQADAgADeAADOgQ", "pic", "CHANCE:0.2"),
-    ("закон", "цыган"): ("AgACAgIAAyEFAATkLtlfAAJsuWmoK_Y1gTzHlQ5TB78PrhXjmTS9AAIxGGsbAAGBQEnUzMX92AABIxYBAAMCAANtAAM6BA","pic", "TTL:5"),
+    ("пакт", "святая троица", "договорня"): ("AgACAgIAAxkBAAIFSGmoS7VnViJHELpV3gR0cMXhLHC7AAJXGGsbeWRASQ80jxNf9hogAQADAgADeAADOgQ", "pic", "CHANCE:0.2"),
+    ("что было", "пары"): ("AgACAgEAAxkBAAIHy2n-DizvsLWqrG9iPPF6zscN3PClAAK4C2sbMlFQR8h0QoZCdm0tAQADAgADeQADOwQ", "pic", "CHANCE:0.2"),
+    ("закон", "цыган"): ("AgACAgIAAyEFAATkLtlfAAJsuWmoK_Y1gTzHlQ5TB78PrhXjmTS9AAIxGGsbAAGBQEnUzMX92AABIxYBAAMCAANtAAM6BA","pic", "TTL:3"),
    
+   #################### ФОРМАТ СТРОКА: СПИСОК
+    "жека": (["AgACAgIAAxkBAAIHqmn-BxgNOannOOC9fLNE16XkSGaPAALvFWsbn5C5SssJzA28LZGIAQADAgADeAADOwQ", "AgACAgIAAxkBAAIHqWn-Bxgy4q1v1Jxm33mIYgYqvCKzAAIIFmsbgq2wShdrZOTYLel4AQADAgADeAADOwQ",
+              "AgACAgIAAxkBAAIHpmn-BxhoBPLO_fHRU6xbB6L1O80bAALhE2sbDo6xShhSYfbGyGbnAQADAgADeAADOwQ", "AgACAgIAAxkBAAIHqGn-BxiiuZ6ALUnkXKc23volCai-AAKcFWsbzgmQSkD6XMD6A1ZgAQADAgADeAADOwQ",
+              "AgACAgIAAxkBAAIHo2n-BxheltRhNpHSeAPW17Z-6NUgAAJxFWsbgq2wSmK5qUU4KuO7AQADAgADeAADOwQ", "AgACAgIAAxkBAAIHpWn-BxjIwcLpbGglwXLJY8q-rCRbAAJyFWsbgq2wSjKePCPEiQhiAQADAgADeAADOwQ",
+              "AgACAgIAAxkBAAIHpGn-Bxg-JOeh0gyHIUcvpdraZBSFAAKQG2sbjpupSpSSvet6o6FjAQADAgADeAADOwQ", "AgACAgIAAxkBAAIHp2n-BxjEPnRMWaESk5VtSPtVD9QdAAInE2sbDo6xSi8C4bBD7PxrAQADAgADeAADOwQ",
+              "AgACAgIAAxkBAAIHx2n-DizqOg0x25nI9U6Y4wtHRYYzAAL-FWsbkqPxSn6Ud0HXNZEdAQADAgADeAADOwQ", "AgACAgIAAxkBAAIHxmn-DiwyrCqUJktZDF2PqL038ZOGAAJMFmsbwErgSl1Dsy6f6VzhAQADAgADeAADOwQ",
+              "AgACAgIAAxkBAAIHyGn-DixKKVnObX2yWLpvfjg3mkGoAALNFWsb9kn4SjUR3fgtnlNKAQADAgADeAADOwQ", "AgACAgIAAxkBAAIICGn-FLFATdldT_rdmPVlcqPwz2iYAALoFWsbkqPxSii1D8dQThG0AQADAgADeAADOwQ",
+              "AgACAgIAAxkBAAIIB2n-FLHIpUFZ95-fId_ZnAfghN_QAAKdFWsbkqPxSvRdHTkZU_wTAQADAgADeAADOwQ", "AgACAgIAAxkBAAIICWn-FLGBgE0YNm59-vZACB2rzmNsAALyFWsbkqPxSpSL3TKKpEh9AQADAgADeAADOwQ"
+              ], "pic"),
+    "слив": (["AgACAgIAAxkBAAIHq2n-BxinzynuxDwLvV1MAAGx7GvyjgACOhhrG9LsyUsT9YCjSr40HgEAAwIAA3gAAzsE", "AgACAgIAAxkBAAIHrGn-Bxiq-JzMzpX8FGbMjcZP08TkAAKqEmsbffbYS5cPyzv3SDhAAQADAgADeAADOwQ",
+              "AgACAgIAAxkBAAIHrmn-BxiPCu0DT0VCnhKbZYJhKC24AAIHGmsbvyHQS8HAYeRCs1VcAQADAgADeAADOwQ", "AgACAgIAAxkBAAIHrWn-BxgS8RNR7DI5QzI0rlZgmagjAAILGGsb0uzJS2fp-2_Ct3yuAQADAgADeAADOwQ"
+              ], "pic", "CHANCE:0.3"),
+
    #################### ФОРМАТ СТРОКА: СТРОКА
+    "отмен": ("AgACAgIAAxkBAAIHnmn-BxgoNTGE5gj8rfR6I3YAAaFjPAACYxZrG84JoEpWXFY8OTZvlAEAAwIAA3gAAzsE", "pic", "IS_REPLY", "CHANCE:0.7"),
     "свагобщага": ("AgACAgIAAxkBAAIFRmmoS5gP3v9FdrNbWbMzKWuJZzeEAAJVGGsbeWRASR0XncSOGXHiAQADAgADeQADOgQ", "pic", "CHANCE:0.7"),
+    "свагагенератор": ("AgACAgIAAxkBAAIHn2n-Bxi-vh5_-zJfQgHJroLuA4gXAAJkFmsbzgmgSvBb2GClBeqLAQADAgADbQADOwQ", "pic", "CHANCE:0.7"),
+    "мяу": ("AgACAgIAAxkBAAIHxWn-Dix-psJwGL_Q-WS8SDnCW7LyAAJvFmsbZoyxSjrbRiL254RaAQADAgADeAADOwQ", "pic", "CHANCE:0.4"),
+    "патрик": ("AgACAgIAAxkBAAIH02n-Diw5qGfQNHkeSU6XMeExOLtjAAKqE2sbgn-xS3SPiqxWSFTKAQADAgADeAADOwQ", "pic", "CHANCE:0.4"),
+    "износ": ("AgACAgIAAxkBAAIH0Gn-DiyG2LJxYcss4_bDdxOTsqQGAAJSFmsbgn-pS2P1vzmlhqoyAQADAgADeQADOwQ", "pic", "CHANCE:0.3"),
+    "рун": ("AgACAgIAAxkBAAIH2Gn-DiwAAXQV9jzpx6xMa1m1-jCojgAC1hNrG_UEuEvafi2ak93sCwEAAwIAA3gAAzsE", "pic", "CHANCE:0.2"),
+    "паблик": ("AgACAgEAAxkBAAIHzGn-DiyBqwEw_f2Ah2CrcEYiZVcCAAK7C2sbMlFQR1Serq1-aHivAQADAgADeAADOwQ", "pic", "CHANCE:0.1"),
+    "наследник": ("AgACAgIAAxkBAAIHnWn-BxiHG3FWzEcRvcUm4NtYHZJ5AAJyFWsbzgmgSuxPrqK9yYkYAQADAgADeQADOwQ", "pic", "CHANCE:0.1"),
+    "вайб": ("AgACAgIAAxkBAAIHoGn-Bxg8EV_dVCjzfzaU56UX57qFAAJiFmsbzgmgSjxHu4QFuZY4AQADAgADeAADOwQ", "pic"),
     "маким": ("AgACAgIAAxkBAAIFOGmoSkG9iHzif-LcBkGEvYBKZq6gAAKxDGsbX5HISz6nimZJVvycAQADAgADeAADOgQ", "pic"),
     "растяпа": ("AgACAgIAAxkBAAIFQGmoSnEcPO9V0LTg8IEFKQbwr9TSAAIkDmsbzf8BSHz9iFxyu_oqAQADAgADeQADOgQ", "pic"),
     "аргументация": ("AgACAgIAAx0Cct4JbwABDO0eaagwxIPCoQ-clBEPpHapEA-D3jgAAmYWaxv1AUBJZV0PxkW7p90BAAMCAAN4AAM6BA", "pic"),
     "чилл": ("AgACAgIAAx0Cct4JbwABDOd8aadFMBOIGNI7U-LInFzrlWV0CIEAArASaxugEZhIQeXoXTaKonABAAMCAAN4AAM6BA", "pic"),
-    "король сваги": ("AgACAgIAAx0Cct4JbwABDOvzaaf3r0SL-QXO_2YdTZfXx8f4SYsAAiYTaxv1AUBJvaIDV6yrw4sBAAMCAAN4AAM6BA", "pic"),
+    "инсульт": ("AgACAgIAAxkBAAIHxGn-DiyZJvzPJmWEGTM8hC06ccsFAAK_F2sbzZ2oSu1g1sEaBw1vAQADAgADbQADOwQ", "pic"),
+    "канибализм": ("AgACAgIAAxkBAAIHzmn-Diz7oWsxuVFGskC3sRd-8dkGAAJeGmsbr8QxSzaqmjBd9CTNAQADAgADeAADOwQ", "pic"),
+    "король сваг": ("AgACAgIAAx0Cct4JbwABDOvzaaf3r0SL-QXO_2YdTZfXx8f4SYsAAiYTaxv1AUBJvaIDV6yrw4sBAAMCAAN4AAM6BA", "pic"),
+    "отличного сваг": ("AgACAgIAAxkBAAIH0mn-DiwE1GxbNVBOGmWSLebbqntzAAKFE2sbgn-xSwYFf82hY15HAQADAgADeAADOwQ", "pic"),
+    "до связи": ("AgACAgIAAxkBAAIHw2n-Diw-iOXH5BT7XuCcBX1FZGZ3AALdE2sbkkiZSkkNphuzPrkAAQEAAwIAA3gAAzsE", "pic"),
+    "бурятская свага": ("AgACAgIAAxkBAAIHoWn-Bxh7FFQQXqgAAeAsLbhB3oQAAW4AArcUaxuF4LBKVl6e1rUqZucBAAMCAAN4AAM7BA", "pic"),
+
 #————————————————————————————————————————————————————— ОТВЕТЫ ЗВУКАМИ ————————————————————————————————————————————————————————————————————————————————————————————————
     #################### ФОРМАТ СПИСОК: СТРОКА
     ("восстали машины", "восстание машин", "киборг убийца", "киборг-убийца"): ("CQACAgIAAxkBAAIFMGmoSYpmHp53gjn8oKXAg3Q-NrMoAALIhwACJ67YS8NSI3V1y7NyOgQ", "audio"),
@@ -148,8 +198,8 @@ ALL_PASHALKO = {
     
     #################### ФОРМАТ СПИСОК: СТРОКА
     ("делаем грязь", "делай грязь", "делай сука ветер"): ("AwACAgIAAx0Cct4JbwABDO0ZaagvcI4T6JkaqyJyores8XI_hKwAAr2VAAL1AUBJqwm_6OsQcJs6BA", "voice", "IS_REPLY"),
-    ("каденза", "каденс", "кейденс"): ("AwACAgIAAxkBAAIFLmmoSXfYsFhJCcFJDU_WHjLpSl0kAAIHlwAC7M4gSG4XNTul_UcuOgQ", "voice", "NEED_REPLY", "IS_REPLY"),
+    ("каденза", "каденс", "кейденс"): ("AwACAgIAAxkBAAIFLmmoSXfYsFhJCcFJDU_WHjLpSl0kAAIHlwAC7M4gSG4XNTul_UcuOgQ", "voice", "IS_REPLY", "CHANCE:0.7"),
 
     #################### ФОРМАТ СТРОКА: СТРОКА
-    "почему я яблоко": ("", "voice", "IS_REPLY")
+    "почему я яблоко": ("AwACAgIAAxkBAAIHl2n-AS8mrJ0yh7ll1y75DAzuh-6QAAJAXwACnn9oSXDGxiJC_0ghOwQ", "voice", "IS_REPLY")
 }
